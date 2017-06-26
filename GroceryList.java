200e4e58a38f7b065d19eaa9edcd3155fec7c896
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 11:38:27 -0700

    add prices and find total by adding together using for loop

--- GroceryList.java
+++ GroceryList.java
@@ -1,9 +1,17 @@
 public class GroceryList {
   public static void main(String[] args) {
-    String[] myGroceryList = { "ice cream", "hot sauce", "pickles" };
+    String[] myGroceryList = { "ice cream", "hot sauce", "pickles", "bananas", "cereal", "rice", "yogurt"  };
+    Integer[] groceryItemPrices = { 3, 6, 4, 2, 4, 4 };
 
-    for ( String groceryItem : myGroceryList) {
-      System.out.println(groceryItem);
+    for ( String groceryItem : myGroceryList ) {
+     System.out.println( groceryItem );
     }
+
+   Integer total = 0;
+    for ( Integer price : groceryItemPrices) {
+      total += price;
+    }
+
+    System.out.println("Your total for this shopping trip will be: $" + total);
   }
 }
