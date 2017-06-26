377b371f98e182b2c96090efe2053b5400edd9d8
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 11:33:27 -0700

    print out each item in grocery array

---
+++ GroceryList.java
@@ -0,0 +1,9 @@
+public class GroceryList {
+  public static void main(String[] args) {
+    String[] myGroceryList = { "ice cream", "hot sauce", "pickles" };
+
+    System.out.println(myGroceryList[0]);
+    System.out.println(myGroceryList[1]);
+    System.out.println(myGroceryList[2]);
+    }
+}
