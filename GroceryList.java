2e215169b9fe7e5347b4910262e8f2a0cd5c51a1
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 11:35:23 -0700

    print out list using for loop

--- GroceryList.java
+++ GroceryList.java
@@ -2,8 +2,8 @@ public class GroceryList {
   public static void main(String[] args) {
     String[] myGroceryList = { "ice cream", "hot sauce", "pickles" };
 
-    System.out.println(myGroceryList[0]);
-    System.out.println(myGroceryList[1]);
-    System.out.println(myGroceryList[2]);
+    for ( String groceryItem : myGroceryList) {
+      System.out.println(groceryItem);
     }
+  }
 }
